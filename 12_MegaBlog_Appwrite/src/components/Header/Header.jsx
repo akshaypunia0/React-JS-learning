f5ffb86df1db13ec6d2a9379a46cfe3e6858b513
akshaypunia0 <akshaypoonia00000@gmail.com>
2025-01-11 11:05:11 +0530

    Create UI conponents

--- 12_MegaBlog_Appwrite/src/components/Header/Header.jsx
+++ 12_MegaBlog_Appwrite/src/components/Header/Header.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import {  Container, Logo, LogoutBtn } from '../index.js'
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+
+
 
 function Header() {
+
+  const authStatus = useSelector((state) => state.auth.status)
+  const navigate = useNavigate()
+
+  const navItem = [
+    {
+      name: 'Home',
+      slug: '/',
+      active: true
+    },
+    {
+      name: 'Login',
+      slug: '/login',
+      active: !authStatus
+    },
+    {
+      name: 'Signup',
+      slug: '/signup',
+      active: !authStatus
+    },
+    {
+      name: 'All posts',
+      slug: '/all-posts',
+      active: authStatus
+    },
+    {
+      name: 'Add post',
+      slug: '/add-post',
+      active: authStatus
+    },
+  ]
+
+
   return (
-    <div>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container />
+    </header>
   )
 }
 
